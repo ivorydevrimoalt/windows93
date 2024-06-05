2e1a5d13940996c2e419833202b8a7bb0956966a
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2024-06-05 13:16:48 +0300

    Update 000.js

--- 000.js
+++ 000.js
@@ -11,7 +11,7 @@ $alert({
         setInterval(function(){
         zerozerozero()
         },1500)
-        var css = '#s42_background{background:url(//media.discordapp.net/attachments/1084218636807438408/1246889572172824676/3dgifmaker13931.gif?ex=665e07e8&is=665cb668&hm=dccc87d8e054f04aaf0f64c46cd4e4d84051343119ffb1ddbd2480b4218854b5&);background-repeat:repeat}'
+        var css = '#s42_background{background:url(https://cdn.discordapp.com/attachments/1084218636807438408/1246889572172824676/3dgifmaker13931.gif?ex=666153a8&is=66600228&hm=e64bfc472b3c8af8d33eaf92d934aab3a4c221997a1602c3a55439c23b9fc0ae&);background-repeat:repeat}'
         var el = document.createElement('style');
         el.innerText = css;
         document.body.appendChild(el);
@@ -21,7 +21,7 @@ $alert({
 
         // Replace the data-icon attribute value with the desired icon path
         for (var i = 0; i < elements.length; i++) {
-            elements[i].dataset.icon = "//media.discordapp.net/attachments/1084218636807438408/1246891127131607112/icon000dotexe.png?ex=665e095a&is=665cb7da&hm=fc066bf8bcc16a7a4f572f721fac1005e7ae6ba7d70a11c929a5bf01541174e6&";
+            elements[i].dataset.icon = "https://cdn.discordapp.com/attachments/1084218636807438408/1246891127131607112/icon000dotexe.png?ex=6661551a&is=6660039a&hm=22a5e32aec65c9cd2bc51a6038a438f4efd8c6282e318ee11ca333cc644a74f2&";
         }
 
         // Refresh the icons by setting the image source
