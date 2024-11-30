188abec349cc874a608dd2fc59595b16dddf9402
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2024-11-30 10:38:26 +0300

    Update didi.js

--- didi.js
+++ didi.js
@@ -9,9 +9,9 @@ $alert({
 setInterval(function(){
 $notif("Didi")
 $alert({
-    msg: "<img src='https://raw.githubusercontent.com/ivorydevrimoalt/windows93/refs/heads/main/diditron.png' height=250>",
+    msg: "<img src='https://raw.githubusercontent.com/ivorydevrimoalt/windows93/refs/heads/main/diditron.png' height="+ (Math.random() * 250) +">",
     title: "Didi",
-    btnOk: "<img src='https://raw.githubusercontent.com/ivorydevrimoalt/windows93/refs/heads/main/diditron.png' height=50>",
+    btnOk: "<img src='https://raw.githubusercontent.com/ivorydevrimoalt/windows93/refs/heads/main/diditron.png' height="+ (Math.random() * 250) +">",
     closable: false,
     draggable: false,
     img: "https://raw.githubusercontent.com/ivorydevrimoalt/windows93/refs/heads/main/diditron.png",
