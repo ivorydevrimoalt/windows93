1a9aca907c3ee15b89212f92f4d6e7adea296054
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2024-11-30 10:11:39 +0300

    Update didi.js

--- didi.js
+++ didi.js
@@ -1,20 +1,20 @@
 $alert({
-    msg: "<img src='https://onakro.com.tr/wp-content/uploads/2022/06/didi-limon.jpg' height=250>",
+    msg: "<img src='diditron.png' height=250>",
     title: "Didi",
-    btnOk: "<img src='https://onakro.com.tr/wp-content/uploads/2022/06/didi-limon.jpg' height=50>",
+    btnOk: "<img src='diditron.png' height=50>",
     closable: false,
     draggable: false,
-    img: "https://onakro.com.tr/wp-content/uploads/2022/06/didi-limon.jpg",
+    img: "diditron.png",
     onok: () => {
 setInterval(function(){
 $notif("Didi")
 $alert({
-    msg: "<img src='https://onakro.com.tr/wp-content/uploads/2022/06/didi-limon.jpg' height=250>",
+    msg: "<img src='diditron.png' height=250>",
     title: "Didi",
-    btnOk: "<img src='https://onakro.com.tr/wp-content/uploads/2022/06/didi-limon.jpg' height=50>",
+    btnOk: "<img src='diditron.png' height=50>",
     closable: false,
     draggable: false,
-    img: "https://onakro.com.tr/wp-content/uploads/2022/06/didi-limon.jpg",
+    img: "diditron.png",
     onok: () => {}
 })
 },1); // Initial value
